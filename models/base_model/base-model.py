387beaf7c98a1093b2e05111196b541815cbcbeb
Leong Hong Fai <leonghongfai@gmail.com>
2024-04-18 17:56:28 +0800

    Correct to include E input

--- models/base_model/base-model.py
+++ models/base_model/base-model.py
@@ -34,19 +34,25 @@ Example of how to use the NliDataset class to create a DataLoader
 tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
 train_encodings_1 = tokenizer(X_train['Sentence1'].astype(str).tolist(), truncation=True, padding=True)
 train_encodings_2 = tokenizer(X_train['Sentence2'].astype(str).tolist(), truncation=True, padding=True)
+train_encodings_3 = tokenizer(X_train['Explanation_1'].astype(str).tolist(), truncation=True, padding=True)
 test_encodings_1 = tokenizer(X_test['Sentence1'].astype(str).tolist(), truncation=True, padding=True)
 test_encodings_2 = tokenizer(X_test['Sentence2'].astype(str).tolist(), truncation=True, padding=True)
+test_encodings_3 = tokenizer(X_test['Explanation_1'].astype(str).tolist(), truncation=True, padding=True)
 
 # Create DataLoader
 train_dataset_1 = NliDataset(train_encodings_1, y_train.tolist())
-train_loader_1 = DataLoader(train_dataset_1, batch_size=16)
+train_loader_1 = DataLoader(train_dataset_1, batch_size = 16)
 train_dataset_2 = NliDataset(train_encodings_2, y_train.tolist())
-train_loader_2 = DataLoader(train_dataset_2, batch_size=16)
+train_loader_2 = DataLoader(train_dataset_2, batch_size = 16)
+train_dataset_3 = NliDataset(train_encodings_3, y_train.tolist())
+train_loader_3 = DataLoader(train_dataset_3, batch_size = 16)
 
 test_dataset_1 = NliDataset(test_encodings_1, y_test.tolist())
 test_dataset_2 = NliDataset(test_encodings_2, y_test.tolist())
-test_loader_1 = DataLoader(test_dataset_1, batch_size=16)
-test_loader_2 = DataLoader(test_dataset_2, batch_size=16)
+test_dataset_3 = NliDataset(test_encodings_3, y_test.tolist())
+test_loader_1 = DataLoader(test_dataset_1, batch_size = 16)
+test_loader_2 = DataLoader(test_dataset_2, batch_size = 16)
+test_loader_3 = DataLoader(test_dataset_3, batch_size = 16)
 
 """
 InferSent Model Architecture with a BiLSTM Encoder and a MLPClassifier
@@ -82,13 +88,8 @@ class BiLSTMEncoder(nn.Module):
         output, _ = nn.utils.rnn.pad_packed_sequence(padded_output,
                                                      batch_first=True)
 
-        if self.maxpool:
-            max_vecs = [torch.max(x, 0)[0] for x in output]
-            embed = torch.stack(max_vecs, 0)
-        else:
-            embed = torch.cat(
-                (output[:, -1, :self.hidden_dim], output[:, -1, self.hidden_dim:]),
-                dim=1)
+        max_vecs = [torch.max(x, 0)[0] for x in output]
+        embed = torch.stack(max_vecs, 0)
         return embed
 
 class Classifier(nn.Module):
@@ -100,28 +101,34 @@ class Classifier(nn.Module):
         self.relu = nn.ReLU()
         self.net = nn.Sequential(self.lin1, self.relu, self.lin2, self.relu, self.lin3)
 
-    def forward(self, premise, hypothesis):
-        combined = torch.cat((premise, hypothesis, torch.abs(premise - hypothesis),
+    def forward(self, premise, hypothesis, explanation):
+        combined = torch.cat((premise, hypothesis, torch.abs(premise - hypothesis), torch.abs(premise - explanation), torch.abs(hypothesis - explanation),
+                              premise * explanation, hypothesis * explanation,
                               premise * hypothesis), 1)
         out = self.net(combined)
         return out
 
 class InferSent(nn.Module):
-    def __init__(self, enc_hidden_dim, cls_hidden_dim, train_loader_1, train_loader_2, num_epochs=2):
+    def __init__(self, enc_hidden_dim, cls_hidden_dim, train_loader_1, train_loader_2, train_loader_3, test_loader_1, test_loader_2, test_loader_3, num_epochs=2):
         super(InferSent, self).__init__()
         self.encoder = BiLSTMEncoder(enc_hidden_dim, maxpool=True)
         self.cls_input_dim = enc_hidden_dim * 2 * 4
         self.classifier = Classifier(self.cls_input_dim, cls_hidden_dim, out_dim=3)
         self.train_loader_1 = train_loader_1
         self.train_loader_2 = train_loader_2
+        self.train_loader_3 = train_loader_3
+        self.test_loader_1 = test_loader_1
+        self.test_loader_2 = test_loader_2
+        self.test_loader_3 = test_loader_3
         self.num_epochs = num_epochs
         self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
     def forward(self, batch):
-        (premise, hypothesis) = batch
+        (premise, hypothesis, explanation) = batch
         premise_encoded = self.encoder(premise)
         hypothesis_encoded = self.encoder(hypothesis)
-        out = self.classifier(premise_encoded, hypothesis_encoded)
+        explanation_encoded = self.encoder(explanation)
+        out = self.classifier(premise_encoded, hypothesis_encoded, explanation_encoded)
         return out
 
     def train_model(self, optimizer, scheduler):
@@ -132,16 +139,19 @@ class InferSent(nn.Module):
             self.train()
             running_loss = 0.0
             
-            for step, (batch1, batch2) in enumerate(zip(self.train_loader_1, self.train_loader_2)):
+            for step, (batch1, batch2, batch3) in enumerate(zip(self.train_loader_1, self.train_loader_2, self.train_loader_3)):
                 b_input_ids = batch1['input_ids'].to(self.device)
                 b_input_mask = batch1['attention_mask'].to(self.device)
                 b_labels = batch1['labels'].to(self.device)
                 b_input_ids2 = batch2['input_ids'].to(self.device)
                 b_input_mask2 = batch2['attention_mask'].to(self.device)
                 b_labels2 = batch2['labels'].to(self.device)
+                b_input_ids3 = batch3['input_ids'].to(self.device)
+                b_input_mask3 = batch3['attention_mask'].to(self.device)
+                b_labels3 = batch3['labels'].to(self.device)
 
                 optimizer.zero_grad()
-                outputs = self((b_input_ids, b_input_ids2))
+                outputs = self((b_input_ids, b_input_ids2, b_input_ids3))
                 loss = criterion(outputs, b_labels)
                 loss.backward()
                 optimizer.step()
@@ -154,11 +164,12 @@ class InferSent(nn.Module):
     def evaluate_model(self):
         self.eval()
         predictions, true_labels = [], []
-        for batch1, batch2 in zip(self.test_loader_1, self.test_loader_2):
+        for batch1, batch2, batch3 in zip(self.test_loader_1, self.test_loader_2, self.test_loader_3):
             b1 = batch1['input_ids'].to(self.device)
             b2 = batch2['input_ids'].to(self.device)
+            b3 = batch3['input_ids'].to(self.device)
             with torch.no_grad():
-                outputs = self((b1, b2))
+                outputs = self((b1, b2, b3))
             logits = outputs.detach().cpu().numpy()
             label_ids = batch1['labels'].to('cpu').numpy()
             predictions.append(logits)
@@ -176,7 +187,7 @@ class InferSent(nn.Module):
         print("Recall:", recall)
         print("F1 Score:", f1)
 
-model = InferSent(1028, 512, train_loader_1, train_loader_2, test_loader_1, test_loader_2).to(device)
+model = InferSent(1028, 512, train_loader_1, train_loader_2, train_loader_3, test_loader_1, test_loader_2, test_loader_3).to(device)
 
 # Set up optimizer and scheduler
 optimizer = AdamW(model.parameters(), lr=5e-5, eps=1e-8)
